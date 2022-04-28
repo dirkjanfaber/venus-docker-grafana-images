33f481ee99a6c3e59ae3114327a54377fede13c0
Dirk-Jan Faber <dfaber@victronenergy.com>
2022-04-28 16:00:25 +0200

    Try to upgrade webpack
    - To version 5, while leaving everyting as much intact as possible.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,111 +1,58 @@
-const webpack = require('webpack')
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const extractCSS = new ExtractTextPlugin('[name].fonts.css')
-const extractSCSS = new ExtractTextPlugin('[name].styles.css')
-
-const BUILD_DIR = path.resolve(__dirname, 'public')
-const SRC_DIR = path.resolve(__dirname, 'src')
-
-console.log('BUILD_DIR', BUILD_DIR)
-console.log('SRC_DIR', SRC_DIR)
-
-module.exports = (env = {}) => {
-  return {
-    entry: {
-      index: [SRC_DIR + '/index.js']
-    },
-    output: {
-      path: BUILD_DIR,
-      filename: '[name].bundle.js'
-    },
-    // watch: true,
-    devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
-    devServer: {
-      contentBase: BUILD_DIR,
-      //   port: 9001,
-      compress: true,
-      hot: true,
-      open: true
-    },
+module.exports = {
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              presets: ['react', 'env']
-            }
-          }
-        },
-        {
-          test: /\.html$/,
-          loader: 'html-loader'
-        },
-        {
-          test: /\.(scss)$/,
-          use: ['css-hot-loader'].concat(
-            extractSCSS.extract({
-              fallback: 'style-loader',
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: { alias: { '../img': '../public_src/img' } }
-                },
-                {
-                  loader: 'sass-loader'
-                }
-              ]
-            })
-          )
-        },
-        {
-          test: /\.css$/,
-          use: extractCSS.extract({
-            fallback: 'style-loader',
-            use: 'css-loader'
-          })
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
-          use: [
+        rules: [
             {
-              // loader: 'url-loader'
-              loader: 'file-loader',
-              options: {
-                name: './img/[name].[hash].[ext]'
-              }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ['@babel/env','@babel/preset-react'] },
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'      
+                ]
             }
-          ]
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'file-loader',
-          options: {
-            name: './fonts/[name].[hash].[ext]'
-          }
-        }
-      ]
+        ]
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-      new webpack.NamedModulesPlugin(),
-      extractCSS,
-      extractSCSS,
-      new HtmlWebpackPlugin({
-        inject: true,
-        template: './public_src/index.html'
-      }),
-      new CopyWebpackPlugin([{ from: './public_src/img', to: 'img' }], {
-        copyUnmodified: false
-      })
-    ]
-  }
+        new HtmlWebPackPlugin({
+            template: "./public_src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    mode: 'development'
 }
